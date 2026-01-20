3b0050bc16828953b83597fbfcc054940ac30fd2
Sohype-khaledKhaled <sohype.mop@gmail.com>
2026-01-21 01:18:37 +0200

    add header to layout template

--- src/runtime/components/Listing/Layout.vue
+++ src/runtime/components/Listing/Layout.vue
@@ -92,6 +92,7 @@ function onFilterChange(patch: Record<string, any>) {
     <!-- Content -->
     <main class="md:col-span-9 space-y-4">
       <!-- Title / Toolbar -->
+      <slot #header/>
 
       <Topbar
         :sort-config="sortConfig"
